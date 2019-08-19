08c3170e1174f433b041adbbbbba45391a664e31
fangyuandoit <676176407@qq.com>
2019-08-19 22:06:57 +0800

    python spider

--- python爬虫开发从入门到实战/第四章简单的网页开发/demo05_beautifulsoup4.py
+++ python爬虫开发从入门到实战/第四章简单的网页开发/demo05_beautifulsoup4.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+import requests
 from bs4  import BeautifulSoup
 
+html = requests.get("https://www.cnblogs.com/").content.decode()
 
+soup = BeautifulSoup(html, 'html.parser')
+find = soup.find_all(class_='titlelnk')
+for a in find:
+    print(a.string)
+    print(a['href'])
